d1968991a0ba3c118ec6cbd66476f4b5abc22f7b
Kevin Jones <kjones@financialforce.com>
2021-06-25 14:40:25 +0100

    Add missing Messaging.CustomNotification

---
+++ src/main/java/com/nawforce/runforce/Messaging/CustomNotification.java
@@ -0,0 +1,27 @@
+/*
+ Copyright (c) 2021 Kevin Jones & FinancialForce, All rights reserved.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote products
+    derived from this software without specific prior written permission.
+ */
+package com.nawforce.runforce.Messaging;
+
+import com.nawforce.runforce.System.String;
+import com.nawforce.runforce.System.Set;
+
+@SuppressWarnings("unused")
+public class CustomNotification {
+    public void send(Set<String> users) { throw new java.lang.UnsupportedOperationException();}
+    public void setNotificationTypeId(String id) { throw new java.lang.UnsupportedOperationException();}
+    public void setTitle(String title) { throw new java.lang.UnsupportedOperationException();}
+    public void setBody(String body) { throw new java.lang.UnsupportedOperationException();}
+    public void setTargetId(String targetId) { throw new java.lang.UnsupportedOperationException();}
+    public void setTargetPageRef(String pageRef) { throw new java.lang.UnsupportedOperationException();}
+}
